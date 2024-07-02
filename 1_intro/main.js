c8c3d6c4a748ab54440898ae4f7f66b9d7302530
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-02 22:29:16 +0530

    "Updated gsap animation properties for #box element"

--- 1_intro/main.js
+++ 1_intro/main.js
@@ -1,5 +1,11 @@
-gsap.to("#box", {
-  x: 1233,
+gsap.from("#box", {
+  x: "60rem",
   duration: 1,
   delay: 1,
+  backgroundColor: "blue",
+  scale: .5,
+  rotate: 270,
+  borderRadius:18,
+  repeat:-1,
+  yoyo:true
 });
