e5db86efa620ab1f8353d88ee9f94d180ee0b979
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-03 21:24:55 +0530

    Class 1 Done

--- 1_intro/main.js
+++ 1_intro/main.js
@@ -1,16 +1,19 @@
 let gsapTimeLine = gsap.timeline();
-
 gsapTimeLine.from("#nav h1", {
   y: -30,
   duration: 1.0,
   delay: 0.5,
   opacity: 0,
 });
-
 gsapTimeLine.from("#nav-items a", {
   y: -30,
   duration: 0.2,
   opacity: 0,
   stagger: 1,
 });
-
+gsapTimeLine.from("#centerText h1", {
+  y: 20,
+  duration: 0.5,
+  opacity: 0,
+  scale: 0.2,
+});
