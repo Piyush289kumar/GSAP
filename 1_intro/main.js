a4314a132202bf94aaa4ef30c5348002f67193fa
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-03 21:23:27 +0530

    "Replaced single gsap animation with a timeline containing two separate animations for #nav h1 and #nav-items a elements."

--- 1_intro/main.js
+++ 1_intro/main.js
@@ -1,11 +1,16 @@
-gsap.from("#box", {
-  x: "60rem",
-  duration: 1,
-  delay: 1,
-  backgroundColor: "blue",
-  scale: .5,
-  rotate: 270,
-  borderRadius:18,
-  repeat:-1,
-  yoyo:true
+let gsapTimeLine = gsap.timeline();
+
+gsapTimeLine.from("#nav h1", {
+  y: -30,
+  duration: 1.0,
+  delay: 0.5,
+  opacity: 0,
 });
+
+gsapTimeLine.from("#nav-items a", {
+  y: -30,
+  duration: 0.2,
+  opacity: 0,
+  stagger: 1,
+});
+
