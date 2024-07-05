485385ebf6cfd1e65981d36ccccc37d5fe892add
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-05 23:14:07 +0530

    "Added image with overlay and hover effect to intro page, updated styles and JavaScript to handle cursor pointer behavior"

--- 1_intro/main.js
+++ 1_intro/main.js
@@ -1,9 +1,23 @@
 let main = document.querySelector("#main");
 let cursorPointer = document.querySelector("#cursor-pointer");
+let imageDiv = document.querySelector("#image-div");
 
 main.addEventListener("mousemove", function (dept) {
   gsap.to(cursorPointer, {
     x: dept.x,
     y: dept.y,
+  });
+});
+
+imageDiv.addEventListener("mouseenter", function () {
+  cursorPointer.innerHTML = "View More";
+  gsap.to(cursorPointer, {
+    scale: 2,
+  });
+});
+imageDiv.addEventListener("mouseleave", function () {
+  cursorPointer.innerHTML = "";
+  gsap.to(cursorPointer, {
+    scale: 1,
   });
 });
