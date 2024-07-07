545b38b580493097950af8381634a962e5d0ba57
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-07 21:49:13 +0530

    Side Nav Build

--- NavBar/main.js
+++ NavBar/main.js
@@ -1,6 +1,23 @@
 let menuBtn = document.querySelector("#menuBtn");
 let menuCloseBtn = document.querySelector("#menuCloseBtn");
-
 let sideNavTimeLine = gsap.timeline();
-
-sideNavTimeLine.from()
+sideNavTimeLine.to("#side-nav", {
+  right: 0,
+  duration: 0.7,
+});
+sideNavTimeLine.from("#side-nav h2", {
+  x: 150,
+  duration: 0.3,
+  stagger: 0.28,
+  opacity: 0,
+});
+sideNavTimeLine.from(menuCloseBtn, {
+  opacity: 0,
+});
+sideNavTimeLine.pause();
+menuBtn.addEventListener("click", () => {
+  sideNavTimeLine.play();
+});
+menuCloseBtn.addEventListener("click", () => {
+  sideNavTimeLine.reverse();
+});
