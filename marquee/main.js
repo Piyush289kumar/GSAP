a9ea8e4ae724dbf09eab74ee96025bc37027d310
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-08 23:45:25 +0530

    "Removed HTML elements and JavaScript code related to text animation, added wheel event listener to animate #marquee element"

--- marquee/main.js
+++ marquee/main.js
@@ -1,25 +1,17 @@
-let h1 = document.querySelector("#textID");
-let h1Text = document.querySelector("#textID").textContent;
-let splitedText = h1Text.split("");
-let halfText = Math.floor(splitedText.length / 2);
-(() => {
-  let cletter = "";
-  splitedText.forEach(function (ele, idx) {
-    cletter += `<span class='${
-      idx < halfText ? "firstHalf" : "secondHalf"
-    }'>${ele}</span>`;
-  });
-  h1.innerHTML = cletter;
-})();
-gsap.from("h1 .firstHalf", {
-  y: 150,
-  duration: 0.5,
-  delay: 0.3,
-  stagger: 0.15,
-});
-gsap.from("h1 .secondHalf", {
-  y: 150,
-  duration: 0.5,
-  delay: 0.3,
-  stagger: -0.15,
+window.addEventListener("wheel", function (dept) {
+  if (dept.deltaY > 0) {
+    gsap.to("#marquee", {
+      transform: "translateX(-200%)",
+      duration: 1,
+      repeat: -1,
+      ease: "none",
+    });
+  } else {
+    gsap.to("#marquee", {
+      transform: "translateX(0%)",
+      duration: 1,
+      repeat: -1,
+      ease: "none",
+    });
+  }
 });
