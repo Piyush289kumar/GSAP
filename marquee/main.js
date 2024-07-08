7993de8e61317ae10aac9bb9fe5ad6337876fded
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-08 23:46:51 +0530

    "Updated marquee animation: increased duration to 1.5, added image rotation"

--- marquee/main.js
+++ marquee/main.js
@@ -2,15 +2,23 @@ window.addEventListener("wheel", function (dept) {
   if (dept.deltaY > 0) {
     gsap.to("#marquee", {
       transform: "translateX(-200%)",
-      duration: 1,
+      duration: 1.5,
       repeat: -1,
+      ease: "none",
+    });
+    gsap.to("#marquee img", {
+      rotate: 180,
       ease: "none",
     });
   } else {
     gsap.to("#marquee", {
       transform: "translateX(0%)",
-      duration: 1,
+      duration: 1.5,
       repeat: -1,
+      ease: "none",
+    });
+    gsap.to("#marquee img", {
+      rotate: 0,
       ease: "none",
     });
   }
