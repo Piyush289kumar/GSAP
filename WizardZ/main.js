3f7983800d9edf37ae93fa63f492257e956b2b21
Piyush289kumar <piyushraikwar289@gmail.com>
2024-07-10 11:31:07 +0530

    "Added new case study section, cursor pointer element, and updated JavaScript and CSS files accordingly."

--- WizardZ/main.js
+++ WizardZ/main.js
@@ -1,25 +1,10 @@
-let h1 = document.querySelector("#textID");
-let h1Text = document.querySelector("#textID").textContent;
-let splitedText = h1Text.split("");
-let halfText = Math.floor(splitedText.length / 2);
-(() => {
-  let cletter = "";
-  splitedText.forEach(function (ele, idx) {
-    cletter += `<span class='${
-      idx < halfText ? "firstHalf" : "secondHalf"
-    }'>${ele}</span>`;
+let body = document.body;
+let cursorPointer = document.querySelector("#cursorPointer");
+body.addEventListener("mousemove", function (dept) {
+  console.log(dept.x);
+  gsap.to(cursorPointer, {
+    x: dept.x - 8,
+    y: dept.y - 95,
+    opacity: 1,
   });
-  h1.innerHTML = cletter;
-})();
-gsap.from("h1 .firstHalf", {
-  y: 150,
-  duration: 0.5,
-  delay: 0.3,
-  stagger: 0.15,
-});
-gsap.from("h1 .secondHalf", {
-  y: 150,
-  duration: 0.5,
-  delay: 0.3,
-  stagger: -0.15,
 });
